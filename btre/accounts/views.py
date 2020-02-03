7209a1959dac845c8903202a7024cde70c4a1f4b
Arpik <arpiksargsyan@gmail.com>
2020-02-03 20:00:23 +0400

    Added view functions templates.

--- btre/accounts/views.py
+++ btre/accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def register(request):
+    return render(request, 'accounts/register.htm')
+
+def login(request):
+    return render(request, 'accounts/login.htm')
+
+def logout(request):
+    return redirect('index')
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.htm')
