89deec1dbf7044dd42eaa63c03c6950f54812a7f
Arpik <arpiksargsyan@gmail.com>
2020-02-05 15:35:52 +0400

    Added Contact model.

--- btre/contacts/models.py
+++ btre/contacts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    listing = models.CharField(max_length=200)
+    lsiting_id = models.IntegerField()
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    message = models.TextField(blank=True) # Optional
+    contact_date = models.DateTimeField(default=datetime.now, blank = True)
+    user_id = models.IntegerField(blank=True)
+    
+    def __str__(self):
+        return self.name
+    
