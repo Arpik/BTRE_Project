359c3082ee0f3014679ab0920aa62abec03dbb4a
Arpik <arpiksargsyan@gmail.com>
2020-02-05 11:38:58 +0400

    Added form fields values.

--- btre/accounts/views.py
+++ btre/accounts/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages 
 
 def register(request):
-    return render(request, 'accounts/register.htm')
+    if request.method == 'POST':
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        # Register User 
+    else:
+        return render(request, 'accounts/register.htm')
 
 def login(request):
+    if request.method == 'POST':
+        # Login User 
+        return
+    else:
+        return render(request, 'accounts/register.htm')
+
     return render(request, 'accounts/login.htm')
 
 def logout(request):
